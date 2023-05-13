4bb75b3c8391b4ec63b53629dc1e140d75c85f4d
daria-kuznecova <119304279+daria-kuznecova@users.noreply.github.com>
2023-05-13 11:24:41 +0000

    Deploying to gh-pages from @ daria-kuznecova/goit-js-hw-09@d104dd985312906333adb4646e5c8307ec166eed 🚀

---
+++ 01-color-switcher.39a8cf2e.js
@@ -0,0 +1,2 @@
+console.log("Hi"),document.querySelector("button[data-start]").addEventListener("click",(function(){console.log("Hi")}));
+//# sourceMappingURL=01-color-switcher.39a8cf2e.js.map
---
+++ 01-color-switcher.fcf2563b.js
@@ -0,0 +1,2 @@
+console.log("Hi");document.querySelector("button[data-start]").addEventListener("click",(()=>{console.log("Hi")}));
+//# sourceMappingURL=01-color-switcher.fcf2563b.js.map
