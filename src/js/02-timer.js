c2f8390d8b2ed1e4d478960f31124fb4712cbc79
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-14 17:23:45 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,20 +4,29 @@ import 'flatpickr/dist/flatpickr.min.css';
 const input = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
 
-function flatpickr(input, options) {
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      if (selectedDates[0] <= Date.now()) {
-        window.alert('Please choose a date in the future');
-      }
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      window.alert('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
       console.log(selectedDates[0]);
-    },
-  };
-}
+      btnStart.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+const targetDate = new Date('Jan 1, 2024 00:00:00');
+const currentDataInMs = Date.now();
+const targetDateInMs = targetDate.getTime();
+const distanceToTargetDate = targetDateInMs - currentDataInMs;
+console.log(distanceToTargetDate);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+const inputDelay = document.querySelector('input[delay]');
+const inputStep = document.querySelector('input[step]');
+const inputAmount = document.querySelector('input[amount]');
+const submitBtn = document.querySelector('button');
+
+const onClick = () => {
+  const delay = [],
+  
+    
+    setInterval(() => { },
+      delay);
+};
+
+submitBtn.addEventListener('click', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
