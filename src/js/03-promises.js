17b4f76afed7a9f8ca15f0066bf02445f56efb26
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-17 15:14:43 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,25 +3,33 @@ const inputStep = document.querySelector('input[step]');
 const inputAmount = document.querySelector('input[amount]');
 const submitBtn = document.querySelector('button');
 
-const everyStep = inputDelay + inputStep * inputAmount;
+// const everyStep = inputDelay + inputStep * inputAmount;
 
-console.log(everyStep);
-
-submitBtn.addEventListener('click', createPromise);
+// console.log(everyStep);
 
 const createPromise = (position, delay) => {
-  const delay = everyStep;
-  const shouldResolve = Math.random() > 0.3;
+  //   // const delay = everyStep;
+  //   // const amount = 0;
+  //   // if (let i = 0, i <= amount, i++) {
+
+  //   // }
+  //
   const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
     setInterval(() => {
       if (shouldResolve) {
         // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
         // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
-    }, delay);
+      console.log(promise);
+    }, 1000);
   });
 };
+// submitBtn.addEventListener('click', createPromise);
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
